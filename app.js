0ab2b2398e06acbe55a6fe1ce3101e0d533bc8d1
dimasasharw <dimas.ashar1997@gmail.com>
2023-07-06 15:53:58 +0700

    coba

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ app.use(express.urlencoded({extended: true}))
 app.get('/', (req, res) => {
     res.render('login')
 })
+app.get('/register', (req, res) => {
+    res.render('register')
+})
+
 
 app.listen(port, () => {
     console.log(`I'm into you <3 :`, port);
